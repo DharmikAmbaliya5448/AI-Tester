b1d026b6ec8e292d79f5075191e3d0689b601411
DharmikAmbaliya5448 <dharmikambaliya153@gmail.com>
2025-08-21 15:58:36 +0530

    Changes in server.js and router.js

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use("/api", router);
 
-const server = app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`✅ Server running on port ${process.env.PORT || 3000}`);
 });
 
-module.exports = { app, server };
+module.exports = app;
